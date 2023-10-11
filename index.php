1e3496a58b7f280091b7c4e3377f0668bfb9bf20
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-11 16:41:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,45 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.1/chart.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: #333;
+            color: #fff;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            background-color: #444;
+            color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        .btn-primary {
+            background-color: #007BFF;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+        }
+
+        .btn-danger {
+            background-color: #DC3545;
+        }
+
+        .btn-danger:hover {
+            background-color: #b82534;
+        }
+
+        .card {
+            background-color: #555;
+            color: #fff;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
@@ -18,6 +56,7 @@
                 <tr>
                     <th>No Data</th>
                     <th>Data</th>
+                    <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
@@ -27,10 +66,31 @@
                 if ($data !== "") {
                     $dataList[] = $data;
                 }
+                if (isset($_GET['action'])) {
+                    if ($_GET['action'] === 'edit') {
+                        $index = $_GET['index'];
+                        if (isset($dataList[$index])) {
+                            $editValue = $dataList[$index];
+                            $editIndex = $index;
+                        }
+                    } elseif ($_GET['action'] === 'delete') {
+                        $index = $_GET['index'];
+                        if (isset($dataList[$index])) {
+                            unset($dataList[$index]);
+                            $dataList = array_values($dataList); // Reindex array
+                        }
+                    } elseif ($_GET['action'] === 'reset') {
+                        $dataList = [];
+                    }
+                }
                 foreach ($dataList as $key => $value) { ?>
                     <tr>
                         <td><?= $key + 1 ?></td>
                         <td><?= $value ?></td>
+                        <td>
+                            <a href="index.php?action=edit&index=<?= $key }" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="index.php?action=delete&index=<?= $key }" class="btn btn-danger btn-sm">Hapus</a>
+                        </td>
                     </tr>
                 <?php } ?>
             </tbody>
@@ -45,26 +105,61 @@
         </div>
         <canvas id="myChart" width="400" height="200"></canvas>
         <canvas id="histogram" width="400" height="200"></canvas>
+        <a href="index.php?action=reset" class="btn btn-danger mt-3">Reset Data</a>
     </div>
 
     <script>
         function calculateMean(dataList) {
-            // Fungsi calculateMean sama seperti yang sebelumnya.
+            if (dataList.length > 0) {
+                let sum = dataList.reduce((acc, val) => acc + val, 0);
+                return (sum / dataList.length).toFixed(2);
+            } else {
+                return 'Tidak ada data';
+            }
         }
 
         function calculateModus(dataList) {
-            // Fungsi calculateModus sama seperti yang sebelumnya.
+            if (dataList.length > 0) {
+                let counts = {};
+                let maxCount = 0;
+                let modus = [];
+
+                for (let data of dataList) {
+                    counts[data] = (counts[data] || 0) + 1;
+                    if (counts[data] > maxCount) {
+                        maxCount = counts[data];
+                        modus = [data];
+                    } else if (counts[data] === maxCount && !modus.includes(data)) {
+                        modus.push(data);
+                    }
+                }
+
+                return modus.join(', ');
+            } else {
+                return 'Tidak ada data';
+            }
         }
 
         function calculateMedian(dataList) {
-            // Fungsi calculateMedian sama seperti yang sebelumnya.
+            if (dataList.length > 0) {
+                let sortedData = dataList.slice().sort((a, b) => a - b);
+                let middle = Math.floor(sortedData.length / 2);
+
+                if (sortedData.length % 2 === 0) {
+                    let median = (sortedData[middle - 1] + sortedData[middle]) / 2;
+                    return median.toFixed(2);
+                } else {
+                    return sortedData[middle].toFixed(2);
+                }
+            } else {
+                return 'Tidak ada data';
+            }
         }
 
         document.getElementById('mean').textContent = calculateMean(<?= json_encode($dataList) ?>);
         document.getElementById('modus').textContent = calculateModus(<?= json_encode($dataList) ?>);
         document.getElementById('median').textContent = calculateMedian(<?= json_encode($dataList) ?>);
 
-        // Membuat grafik persebaran data
         var ctx = document.getElementById('myChart').getContext('2d');
         var dataList = <?= json_encode($dataList) ?>;
         var dataLabels = Array.from({ length: dataList.length }, (_, i) => i + 1);
@@ -98,7 +193,6 @@
             }
         });
 
-        // Membuat histogram
         var histogramCtx = document.getElementById('histogram').getContext('2d');
         var histogram = new Chart(histogramCtx, {
             type: 'bar',
